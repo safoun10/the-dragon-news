70104b83f924ef06c7a01a037fc070ed41ed8bf8
Safoun <safoun.official@gmail.com>
2023-04-27 19:35:26 +0600

    added header with dynamic date and time using moment js

--- src/layout/body/Body.jsx
+++ src/layout/body/Body.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import Header from "./../../pages/shared/header/Header"
-import Footer from '../../pages/shared/footer/Footer';
+import React from "react";
+import Header from "./../../pages/shared/header/Header";
+import Footer from "../../pages/shared/footer/Footer";
+import LeftNav from "../../pages/shared/leftNav/LeftNav";
+import RightNav from "../../pages/shared/rightNav/RightNav";
 
 const Body = () => {
-    return (
-		<div>
+	return (
+		<div style={{maxWidth: "1200px"}} className="mx-auto">
 			<Header></Header>
-			<div className="display-1 text-center">hi (‾◡◝)</div>
-			<Footer></Footer>
+			{/* <section className="row">
+				<div className="col-lg-3">
+					<LeftNav></LeftNav>
+				</div>
+				<div className="col-lg-6">Content Coming Soon</div>
+				<div className="col-lg-3">
+					<RightNav></RightNav>
+				</div>
+			</section> */}
+			{/* <Footer></Footer> */}
 		</div>
 	);
 };
--- src/pages/shared/header/Header.jsx
+++ src/pages/shared/header/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import logo from "../../../assets/logo.png";
+import moment from 'moment/moment';
 
 const Header = () => {
     return (
-        <div>
-            <div>dragon news</div>
-        </div>
-    );
+		<div className="d-flex flex-column justify-content-center align-items-center mt-5">
+			<div>
+				<img className="mb-4 img-fluid" src={logo} />
+			</div>
+			<div className="text-secondary mb-2">
+				Journalism Without Fear or Favor
+			</div>
+			<div className="fs-5">
+				<span>{moment().format("dddd")}</span> , 
+				<span  className='text-secondary'>{moment().format(" MMMM Do YYYY")}</span>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
